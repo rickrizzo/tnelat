0b7800f75a5a70bca0b61d835bd01db8e755ab3c
Robert Russo <rrusso44@gmail.com>
2015-10-18 21:40:01 -0400

    Navigation Bar
    Began creating a navigation bar that can be included on any page with
    bootstrap

---
+++ components/navigation.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<div classs="navbar-header">
+			<a class="navbar-brand" href="#">Tnelat</a>
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="tnelat-nav-collapse" aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+		</div>
+
+		<!--Collapsed in Mobile-->
+		<div class="collapse navbar-collapse" id="tnelat-nav-collapse">
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+				<li><a href="#">Link</a></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#">Login</a></li>
+				<li><a href="#">Signup</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
