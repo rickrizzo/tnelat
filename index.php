7bd99aaa27fe3aa01d336992dfe2fee6b8b266c9
Robert Russo <rrusso44@gmail.com>
2015-10-18 21:40:19 -0400

    Migration to PHP
    All pages should now be made with PHP for use with components

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tnelat</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+	<?php include 'components/navigation.php'; ?>
+	<div class="container">
+		<h1>Welcome to tnelat, a work in progress</h1>
+		<ul>
+			<li><a href="pages/writeReview.php">Write a review</a></li>
+		</ul>
+	</div>
+
+	<!--Resouces-->
+	<script src="js/jquery-2.1.4.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
