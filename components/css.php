6f97fccd16a080c8d8f648a7b42b3ba3c8343aa1
Robert Russo <rrusso44@gmail.com>
2015-11-11 21:59:14 -0500

    JS & CSS Components
    Why repeat yourself?

---
+++ components/css.php
@@ -0,0 +1,3 @@
+<!--CSS-->
+<link rel="stylesheet" type="text/css" href="../css/stylesheet.css">
+<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
---
+++ components/scripts.php
@@ -0,0 +1,3 @@
+<script src="js/jquery-2.1.4.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="//twemoji.maxcdn.com/twemoji.min.js"></script>
