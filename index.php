3e9f2c15f6f6f911d4dc8acb40a59d9a9a03b099
Robert Russo <rrusso44@gmail.com>
2015-12-01 13:12:32 -0500

    Routing
    Basic routing implemented and API structure

--- index.php
+++ index.php
@@ -1,54 +1,16 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Tnelat</title>
-	<?php require 'components/css.php'; ?>
-</head>
-<body>
+<?php
+  require_once "vendor/autoload.php";
 
-	<!--Navigation-->
-	<?php require 'components/navigation.php'; ?>
+  $app = new \Slim\Slim();
 
-	<!--Header-->
-	<header>
-		<div>
-			<h1>tnelat</h1>
-			<h4>Find the team you need</h4>
-		</div>
-	</header>
-	
-	<!--Actions-->
-	<section id="actions">
-		<div>
-			<img src="resources/review.png" alt="">
-			<a class="btn" href="pages/writeReview.php">Review Teammates</a>
-			<article>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis libero blanditiis quo deleniti. Quisquam a esse numquam, nostrum dolorem, cum repellat. Assumenda magnam et non sed voluptatibus, nihil hic tempore.
-			</article>
-		</div>
-		<div>
-			<img src="resources/team.png" alt="">
-			<a class="btn" href="pages/reviews.php">Find Teammates</a>
-			<article>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis libero blanditiis quo deleniti. Quisquam a esse numquam, nostrum dolorem, cum repellat. Assumenda magnam et non sed voluptatibus, nihil hic tempore.
-			</article>
-		</div>
-		<div>
-			<img src="resources/learn.png" alt="">
-			<a class="btn" href="backend/backend.php">Learn More</a>
-			<article>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis libero blanditiis quo deleniti. Quisquam a esse numquam, nostrum dolorem, cum repellat. Assumenda magnam et non sed voluptatibus, nihil hic tempore.
-			</article>
-		</div>
-	</section>
+  //Routes
+  $app->get('/', function () {
+    include "pages/home.php";
+  });
+  $app->get('/review', function() {
+    include "pages/writeReview.php";
+  });
 
-	<!--Footer-->
-	<footer>Created by Dan, Deborah, Theo and Rob</footer>
-
-	<!--Resouces-->
-	<?php require 'components/scripts.php'; ?>
-	<script src="js/frontpage.js"></script>
-</body>
-</html>
+  //Run Application
+  $app->run();
+?>
