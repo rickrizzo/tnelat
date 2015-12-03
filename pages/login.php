78a752e9f4cabb8fea44e976d5d4e50be8baff2a
Deborah Ang <debang35@gmail.com>
2015-12-03 02:47:50 -0500

    Cleaned up Login page
    Deleted In page CSS and added some comments. Also fixed indentations and
    removed forms.

--- pages/login.php
+++ pages/login.php
@@ -13,63 +13,33 @@
 
 	  <!--Navigation Bar-->
   	<?php include '../components/navigation.php'; ?>
-
-  	<!--About-->
-  	<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
-   
-    <style>
-    label {
-      display: inline-block; width: 5em;
-    }
-    fieldset div {
-      margin-bottom: 2em;
-    }
-    fieldset .help {
-      display: inline-block;
-    }
-    .ui-tooltip {
-      width: 210px;
-    }
-    </style>
-
-    <script>
-      $(function() {
-        var tooltips = $( "[title]" ).tooltip({
-          position: {
-            my: "left top",
-            at: "right+5 top-5"
-          }
-        }); 
-      });
-    </script>
   </head>
 
   <body>
+    <!--Login HTML Form-->
+    <section class="log" style="margin-left: auto; margin-right: auto; margin-top: 150px; border: double; width: 380px">
+      <div class="pagewidth">
+        <h2><font color="white">Login </h2>
+        <h5>Don't have an account? <b><a href="signup.php"> Sign up here. </a></b></h5>
+        </font>
+      </div>
+      <fieldset>
+        <section class="pagewidth">
+          <div class="login">
+            <article>
+              <p><input id="username" type="text" name="username" value="" placeholder="Username or Email" required></p>
+              <p><input id="password" type="password" name="password" value="" placeholder="Password" required></p>
+              <p class="submit"><input id="login" type="button" name="commit" value="Login"></p>
+            </article>
+          </div>
+          <div class="login-help">
+            <p><font color="white">Forgot your password? <a href="index.php">Click here to reset it</a>.</p>
+          </div>
+        </section>
+      </fieldset>
+    </section>
 
-    <form class= "log"  method="post" action="login.php" style="margin-left: auto; margin-right: auto; margin-top: 150px; border: double; width: 380px">
-   
-    <div class="container">
-      <h2><font color="white">Login </h2>
-      <h5>Don't have an account? <b><a href="signup.php"> Sign up here. </a></b></h5>
-    </font></div>
-
-    <fieldset>
-      <section class="container" >
-        <div class="login" >
-          <form method="post" action="index.html">
-            <p><input id="username" type="text" name="username" value="" placeholder="Username or Email" required></p>
-            <p><input id="password" type="password" name="password" value="" placeholder="Password" required></p>
-        
-            <p class="submit"><input id="login" type="button" name="commit" value="Login"></p>
-          </form>
-        </div>
-
-        <div class="login-help">
-          <p><font color="white">Forgot your password? <a href="index.php">Click here to reset it</a>.</p>
-        </div>
-      </section>
-    </form>
+    <!--Javascript-->
     <script src='../dan/Post.js'></script>
     <script>
       function myFunction() {
@@ -88,6 +58,5 @@
 
   	<!--Resouces-->
   	<?php include '../components/scripts.php'; ?>
-
   </body>
 </html>
