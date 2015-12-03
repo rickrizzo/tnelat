d10c869b0fd19fb4c5b19730fcd2c5e91d647743
Robert Russo <rrusso44@gmail.com>
2015-12-03 15:35:34 -0500

    Merge Fix and CSS

--- pages/signup.php
+++ pages/signup.php
@@ -11,49 +11,24 @@
 	<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
   <script src="//code.jquery.com/jquery-1.10.2.js"></script>
   <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-  <style>
-  label {
-    display: inline-block; width: 5em;
-  }
-  fieldset div {
-    margin-bottom: 2em;
-  }
-  fieldset .help {
-    display: inline-block;
-  }
-  .ui-tooltip {
-    width: 250px;
-  }
-  </style>
-  <script>
- /* $(function() {
-    var tooltips = $( "[title]" ).tooltip({
-      position: {
-        my: "left top",
-        at: "right+5 top-5"
-      }
-    });)*/
-  </script>
 </head>
 </body>
  
 
 	<!--Form-->
-
-	 <section class="container" >
-   <form class= "log"  method="POST" action="signup.php" style= " margin-left: auto; margin-right: auto; margin-top: 100px; border: double; width: 70%; min-width: 500px; padding: 20px">
+	 <section id="signup" class="pagewidth" >
+   <form class= "log"  method="POST" action="signup.php" >
       <div class="container">
-        <font color="white"> <h2>Sign Up </h2><p> Already a user? <a href="login.php"><b>Login here</b></a></p>
-        <p><input  type="text" id="first_name" name="first_name" value="" placeholder="First Name">  Enter your first name*</b></p> 
-        <p><input type="text" id="last_name" name="last_name" value="" placeholder="Last Name">  Enter your last name*</p>
-        <p><input type="text" id="username" name="username" value="" placeholder="Username">  Create a username*</p>
-        <p><input type="password" id="password" name="password" value="" placeholder="Password">  Create a password*</p>
-        <p><input type="password" id="password_confirm" name="password_confirm" value="" placeholder="Re-Type Password">  Re-type your password*</p>
-        <p><input type="text" id="email" name="email" value="" placeholder="Email Address">  Enter your email address*</p>
-        <p><input type="text" id="phone" name="phone" value="" placeholder="Phone Number">  Enter a reachable phone number</p>
-        <p class="submit"><input type="button" id="submit" name="submit" value="Create Account"><i> * items required</i></p>
+        <font color="white"> <h2>Sign Up </h2><p> Already a user? <a href="login.php"><b>Login here</b></a></p></font>
+        <p><input  type="text" id="first_name" name="first_name" value="" placeholder="First Name"> <b>Enter your first name</b></p> 
+        <p><input type="text" id="last_name" name="last_name" value="" placeholder="Last Name"> <b>Enter your last name</b></p>
+        <p><input type="text" id="username" name="username" value="" placeholder="Username"> <b>Create a username</b></p>
+        <p><input type="password" id="password" name="password" value="" placeholder="Password"> <b>Create a password</b></p>
+        <p><input type="password" id="password_confirm" name="password_confirm" value="" placeholder="Re-Type Password"> <b>Re-type your password</b></p>
+        <p><input type="text" id="email" name="email" value="" placeholder="Email Address"> <b>Enter your email address</b></p>
+        <p><input type="text" id="phone" name="phone" value="" placeholder="Phone Number"> Enter a reachable phone number</p>
+        <p class="btn" type="button" id="submit" name="submit"> Create Account</p>
         <p id="error"> </p>
-        </font>
       </div>
     </form>
   </section>
