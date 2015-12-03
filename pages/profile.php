4792de8ca5432fe0ed2af85a7a8401e126ba18a1
Robert Russo <rrusso44@gmail.com>
2015-12-03 02:52:10 -0500

    Working! (Finally)

--- pages/profile.php
+++ pages/profile.php
@@ -1,32 +1,25 @@
 <?php
-  
   function profile($UID) {
     //Includes
     include 'components/css.php';
     include 'components/connector.php';
     include 'components/scripts.php';
-    //include 'components/navigation.php';
+    include 'components/navigation.php';
     include 'dan/SQL_Operation.php';
 
-    //Connect to DB
-    try {
-      $conn = new PDO('mysql:host=localhost;dbname=tnelat;',$user, $password);
-      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //Variables
+    $userdata = (new GetUser($UID))->execute();
+    $name = ucfirst($userdata[0]['firstname']) . " " . ucfirst($userdata[0]['lastname']);
+    $reviews = (new GetReviewsAbout($UID))->execute();
 
-      //Variables
-      $userdata = (new GetUser($UID))->execute();
-      $name = ucfirst($userdata[0]['firstname']) . " " . ucfirst($userdata[0]['lastname']);
-      $reviews = (new GetReviewsAbout($UID))->execute();
-
-      //Title
-      echo "<!DOCTYPE html><html><head><title>" . $name . "</title></head>";
-      echo "<body><h1 class='jumbotron'>" . $name . "</h1><section><h2>Reviews</h2>";
-      echo '<script>$.getJSON("/tnelat/data/emoji.json", function(data) {$("article").html(data.emoji[' . $reviews[0]['emoji'] . ']); });</script>';
+    //Title
+    echo "<!DOCTYPE html><html><head><title>" . $name . "</title></head>";
+    echo "<body><h1 class='jumbotron'>" . $name . "</h1><section><h2>Reviews</h2>";
+    foreach ($reviews as $review) {
+      echo '<div><script>$.getJSON("/tnelat/data/emoji.json", function(data) {$("article").html(data.emoji[' . $review['emoji'] . ']); });</script>';
       echo "<article></article>";
-      echo "<p>" . $reviews[0]['review']. "</p>";
-      echo "</section></body></html>";
-    } catch (Exception $e) {
-      echo "Error: " . $e->getMessage();
+      echo "<p>" . $review['review']. "</p></div>";
     }
+    echo "</section></body></html>";
   }
 ?>
