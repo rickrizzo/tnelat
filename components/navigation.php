efc68a407263304584661d1bedc4d477087a78dc
Robert Russo <rrusso44@gmail.com>
2015-12-03 15:38:24 -0500

    FIX

--- components/navigation.php
+++ components/navigation.php
@@ -10,8 +10,8 @@
 		</span>
 		<span id="right-menu">
 			<?php
-				if(isset($_SESSION['username'])) {
-					echo '<a href="/tnelat/user/' . $_SESSION['username'] . '"><li>' . $_SESSION['username'] . '</li></a>';
+				if(isset($_SESSION['UID'])) {
+					echo '<a href="/tnelat/user/' . $_SESSION['UID'] . '"><li>' . $_SESSION['username'] . '</li></a>';
 				} else {
 					echo '<a href="/tnelat/login"><li>Login</li></a>' .
 						'<a href="/tnelat/signup"><li>Sign Up</li></a>';
