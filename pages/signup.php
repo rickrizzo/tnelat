3d65094b465c44d7ab527651ed090d1a2ad25e38
Robert Russo <rrusso44@gmail.com>
2015-12-06 11:59:18 -0500

    Minor Login Tweak
    Now you can actually read the errors, pretty helpful

--- pages/signup.php
+++ pages/signup.php
@@ -41,7 +41,9 @@
         PostReq.addParamsById('username', 'password', 'password_confirm', 'first_name', 'last_name', 'email', 'phone');
         PostReq.set_callback( function(val) {
           $('#response').html(val);
-          parent.window.location.reload();
+          if(val.indexOf("successful") != -1) {
+            parent.window.location.reload();
+          }
         });
         PostReq.send();     
       });
