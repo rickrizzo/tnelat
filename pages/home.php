b94fe51f330fa61464d64a81d8a7f41f4f090037
Robert Russo <rrusso44@gmail.com>
2015-12-07 00:52:55 -0500

    Scripts and Filler

--- pages/home.php
+++ pages/home.php
@@ -22,21 +22,21 @@
 				<img src="resources/review.png" alt="">
 				<a class="btn" href="#">Review Teammates</a>
 				<article>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis libero blanditiis quo deleniti. Quisquam a esse numquam, nostrum dolorem, cum repellat. Assumenda magnam et non sed voluptatibus, nihil hic tempore.
+					Write reviews and contribute to the growing Tnelat community. Reward great partners and alert others to work styles that may potentially clash with their own.
 				</article>
 			</div>
 			<div>
 				<img src="resources/team.png" alt="">
 				<a class="btn" href="#">Find Teammates</a>
 				<article>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis libero blanditiis quo deleniti. Quisquam a esse numquam, nostrum dolorem, cum repellat. Assumenda magnam et non sed voluptatibus, nihil hic tempore.
+					Need to get something done? Find your team here.
 				</article>
 			</div>
 			<div>
 				<img src="resources/learn.png" alt="">
 				<a class="btn" href="#">Learn More</a>
 				<article>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis libero blanditiis quo deleniti. Quisquam a esse numquam, nostrum dolorem, cum repellat. Assumenda magnam et non sed voluptatibus, nihil hic tempore.
+					Tnelat is a growing platform. Learn more about how we plan to leverage this tool in the future to create real change in the workplace.
 				</article>
 			</div>
 		</section>
