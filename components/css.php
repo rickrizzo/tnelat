f3ef108ad8038e98af0254a801c5bd38757b439f
Robert Russo <rrusso44@gmail.com>
2015-12-08 02:18:26 -0500

    Removed excess files

--- components/css.php
+++
@@ -1,3 +0,0 @@
-<!--CSS-->
-<link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet" type="text/css">
-<link href="/tnelat/css/stylesheet.css" rel="stylesheet" type="text/css">
--- components/scripts.php
+++
@@ -1,2 +0,0 @@
-<script src="/tnelat/js/jquery-2.1.4.min.js"></script>
-<script src="/tnelat/js/navigation.js"></script>
