9feeb34433c910cd9efa9920e0b23d8aa7c6e728
Robert Russo <rrusso44@gmail.com>
2015-12-10 00:53:01 -0500

    Search On Load
    Now you don’t even need to click

--- js/search.js
+++ js/search.js
@@ -9,10 +9,20 @@ function enable_term() {
 	$('#search_term')[0].style.backgroundColor = '#FFFFFF';
 }
 
+$('#submit').click(function() {
+	PostReq = new Post('handlers/search_engine.php');
+	PostReq.addParamsById('search_by', 'search_term', 'sort_by', 'order');
+	PostReq.set_callback(function(val) {
+		$('#search_results').html(val);
+	});
+	PostReq.send();
+});
+
 $(document).ready(function() {
 	search = $('#search_by');
 	disable_term();
 
+	//Disable Fields
 	search.change(function() {
 		if (search[0].options[search[0].selectedIndex].value == 'all') {
 			disable_term();
@@ -22,12 +32,5 @@ $(document).ready(function() {
 		}
 	});
 
-	$('#submit').click(function() {
-		PostReq = new Post('handlers/search_engine.php');
-		PostReq.addParamsById('search_by', 'search_term', 'sort_by', 'order');
-		PostReq.set_callback(function(val) {
-			$('#search_results').html(val);
-		});
-		PostReq.send();
-	});
+	$('#submit').trigger('click');
 });
