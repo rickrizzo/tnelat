471ff0437e2554dbf2acb58ff9812359b8a43f37
Dan Brenner <brennd3@rpi.edu>
2015-12-09 22:44:35 -0500

    One more change.

--- js/search.js
+++ js/search.js
@@ -1,14 +1,24 @@
+function disable_term() {
+	$('#search_term').attr('disabled', 'disabled');
+	$('#search_term')[0].style.backgroundColor = '#D3D3D3';
+	$('#search_term')[0].value = '';
+}
+
+function enable_term() {
+	$('#search_term').attr('disabled', false);
+	$('#search_term')[0].style.backgroundColor = '#FFFFFF';
+}
+
 $(document).ready(function() {
 	search = $('#search_by');
+	disable_term();
+
 	search.change(function() {
 		if (search[0].options[search[0].selectedIndex].value == 'all') {
-			$('#search_term').attr('disabled', 'disabled');
-			$('#search_term')[0].style.backgroundColor = '#D3D3D3';
-			$('#search_term')[0].value = '';
+			disable_term();
 		}
 		else {
-			$('#search_term').attr('disabled', false);
-			$('#search_term')[0].style.backgroundColor = '#FFFFFF';
+			enable_term();
 		}
 	});
 
